7788a2d3677792972164044b858b293bbab81b08
Nivash Ramachandran <nivash_ramachandran@dell.com>
2019-02-17 16:44:09 +0530

    Remove tab

--- json2css.js
+++ json2css.js
@@ -1,22 +1,16 @@
-(function( $ ) {
- 
-    $.fn.json2css = function(jsonmodel) {
-	
-		var css = "<style>";
-		
-        for (var i in jsonmodel) {
-            css += " " + i + "{";
-            var imodel = jsonmodel[i];
-            for (var j in imodel) {
-                css += j + ":" + imodel[j] + ";";
+(function($) { 
+    $.fn.json2css = function(data) {
+        var css = "<style>";
+        for (var istyle in data) {
+            css += " " + istyle + "{";
+            var idata = data[istyle];
+            for (var iprop in idata) {
+                css += iprop + ":" + idata[iprop] + ";";
             }
             css += "}";
         }
-		
         css += "</style>";
-		
         this.append(css);
- 
-        return this; 
-    }; 
-}( jQuery ));
+        return this;
+    };
+}(jQuery));
